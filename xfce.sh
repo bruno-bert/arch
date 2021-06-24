e4a034e832dec91ee14ccb3686b88160ecb4d92c
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-06-24 19:32:08 -0300

    more fixes

--- xfce.sh
+++ xfce.sh
@@ -18,7 +18,12 @@ sudo systemctl enable --now auto-cpufreq
 
 sudo pacman -S --noconfirm xorg xfce4 xfce4-goodies firefox obs-studio vlc xorg-server xorg-xinit
 
+#if on virtual box
+#pacman -S virtualbox-guest-utils virtualbox-guest-modules-arch mesa mesa-libgl
+
+#this script will not isntall any display manager (the login will be made on tty console terminal -> lightweight!)
 #sudo systemctl enable lightdm
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
