60b4b9c574ce5d589c3e99fe8cc43b0a9c642f5d
bruno-bert <bruno.bert.jj@gmail.com>
2021-06-25 18:12:51 -0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -9,10 +9,10 @@ makepkg -si --noconfirm
 
 #install apps from AUR
 /bin/echo -e "\e[1;32mInstalling apps from AUR...\e[0m"
-sudo paru -S --noconfirm vala0.44 vala-panel vala-panel-appmenu-common-git vala-panel-appmenu-registrar-git vala-panel-appmenu-xfce-git auto-cpufreq appmenu-gtk-module-git asdf-vm pamac-aur libpamac-aur mongodb-bin-4.0 notes panther-launcher-git postman-bin visual-studio-code-bin timeshift
+paru -S --noconfirm vala-panel vala-panel-appmenu-common-git vala-panel-appmenu-registrar-git vala-panel-appmenu-xfce-git auto-cpufreq appmenu-gtk-module-git asdf-vm pamac-aur libpamac-aur mongodb-bin-4.0 notes panther-launcher-git postman-bin visual-studio-code-bin timeshift
 
 /bin/echo -e "\e[1;32mInstalling Development Tools from AUR...\e[0m"
-sudo paru -S --noconfirm asdf-vm mongodb-bin-4.0 postman-bin visual-studio-code-bin
+paru -S --noconfirm asdf-vm mongodb-bin-4.0 postman-bin visual-studio-code-bin
 
 
 #install apps from official repos
